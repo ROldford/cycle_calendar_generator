5e6c521e9b37dffda0247609944250f182c6449d
Ryan Oldford <ryan.oldford@gmail.com>
2018-08-15 15:33:17 +0800

    Fixed test bugs related to line separation, added new tests for parseScheduleSetup()

--- tests/test_cycle_calendar_generator.py
+++ tests/test_cycle_calendar_generator.py
@@ -11,7 +11,8 @@ from cycle_calendar_generator import cycle_calendar_generator
 
 
 class Test_get_args(unittest.TestCase):
-    """Tests for `cycle_calendar_generator` package."""
+    # Get directory from args
+    """Tests function to get folder argument and give default if none given"""
 
     def setUp(self):
         """Set up test fixtures, if any."""
@@ -19,26 +20,53 @@ class Test_get_args(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    @mock.patch('cycle_calendar_generator.cycle_calendar_generator.argparse.ArgumentParser.parse_args')
+    @mock.patch(
+        'cycle_calendar_generator.cycle_calendar_generator.argparse.ArgumentParser.parse_args'
+    )
     def test_if_arg_is_string(self, mock_parse_args):
         """Normal input on command line"""
-        mock_parse_args.return_value = cycle_calendar_generator.argparse.Namespace(folder='string')
+        mock_parse_args.return_value = cycle_calendar_generator.argparse.Namespace(
+            folder='string'
+        )
         self.assertIsInstance(cycle_calendar_generator.getArgs(), str)
 
-    @mock.patch('cycle_calendar_generator.cycle_calendar_generator.argparse.ArgumentParser.parse_args')
+    @mock.patch(
+        'cycle_calendar_generator.cycle_calendar_generator.argparse.ArgumentParser.parse_args'
+    )
     @mock.patch('cycle_calendar_generator.cycle_calendar_generator.getcwd')
     def test_gets_current_dir_if_no_arg_given(self, mock_getcwd, mock_parse_args):
         """Use current working directory if no folder given"""
         cwd_string = 'current working directory'
-        mock_parse_args.return_value = cycle_calendar_generator.argparse.Namespace(folder=None)
+        mock_parse_args.return_value = cycle_calendar_generator.argparse.Namespace(
+            folder=None
+        )
         mock_getcwd.return_value = cwd_string
         self.assertEqual(cycle_calendar_generator.getArgs(), cwd_string)
 
+class Test_parse_schedule_setup_file(unittest.TestCase):
+    # Check for schedule setup Excel file
+    # Open and parse schedule setup file (checking for valid data)
+    # Parsing should generate:
+    ## [dict]periodTiming -> [int]periodNumber: [tuple(Date, Date)](startTime, endTime)
+    ## [list]CycleDaysList -> (str)cycleDay {showing all cycleDay strings}
+    ## [dict]yearlySchedule -> [Date]date: [str]cycleDay
+    """Tests function to open and parse schedule setup Excel file"""
+
+    def test_opens_and_parses_correct_file(self):
+        """Expected behavior: finds file with preset filename, opens and parses,
+        returning object containing setup dicts and lists"""
+        pass
+
+    @mock.patch('cycle_calendar_generator.cycle_calendar_generator.listdir')
+    def test_passes_exception_if_no_setup_file(self, mock_listdir):
+        """If no Excel file matching preset filename exists, throw ValueError"""
+        mock_listdir.return_value = ['TeacherOne.xlsx', 'TeacherTwo.xlsx']
+        self.assertRaises(ValueError, cycle_calendar_generator.parseScheduleSetup)
 
 
-# Get directory from args
-# Check for schedule setup Excel file
-# Open and parse schedule setup file (checking for valid data)
+
+
+
 # Parsing should generate:
 ## [dict]periodTiming -> [int]periodNumber: [tuple(Date, Date)](startTime, endTime)
 ## [list]CycleDaysList -> (str)cycleDay {showing all cycleDay strings}
