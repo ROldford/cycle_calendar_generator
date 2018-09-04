79cecb527e39f87f315fcbff7281270db9d59de4
Ryan Oldford <ryan.oldford@gmail.com>
2018-09-04 16:10:35 +0800

    Added tests for ValueErrors generated when parsing bad setup files

--- tests/test_cycle_calendar_generator.py
+++ tests/test_cycle_calendar_generator.py
@@ -7,6 +7,7 @@
 import unittest
 from unittest import mock
 import os
+import datetime
 
 from pyfakefs import fake_filesystem_unittest
 
@@ -54,7 +55,7 @@ class Test_parse_schedule_setup_file(fake_filesystem_unittest.TestCase):
     ## [list]CycleDaysList -> (str)cycleDay {showing all cycleDay strings}
     ## [dict]yearlySchedule -> [Date]date: [str]cycleDay
     """Tests function to open and parse schedule setup Excel file"""
-
+    # Setting up folder paths for fakefs
     good_folder_path = ''
     bad_folder_path = ''
     not_a_folder_path = ''
@@ -65,6 +66,45 @@ class Test_parse_schedule_setup_file(fake_filesystem_unittest.TestCase):
     )
     bad_folder_full_filepath_list = []
     good_folder_full_filepath_list = []
+    # Setting up good and bad Excel files
+    data_periodTiming = [
+      ["Period Number", "Start Time", "End Time"],
+      ["1", "08:00 AM", "09:00 AM"],
+      ["2", "09:00 AM", "10:00 AM"],
+      ["3", "10:00 AM", "11:00 AM"],
+      ["4", "11:00 AM", "12:00 PM"],
+      ["5", "12:00 PM", "01:00 PM"],
+    ]
+    data_cycleDaysList = ["A1", "B2", "C3", "D4", "E5", "F6"]
+    data_yearlySchedule = [
+      ["August 31", data_cycleDaysList[0]],
+      ["September 3", data_cycleDaysList[1]],
+      ["September 4", data_cycleDaysList[2]],
+      ["September 5", data_cycleDaysList[3]],
+      ["September 6", data_cycleDaysList[4]],
+      ["September 7", data_cycleDaysList[5]],
+    ]
+    data_badExcel = [
+      ["This", "isn't", "the", "right"],
+      ["data", "for", "the", "parser"]
+    ]
+    data_justATextDoc = "This isn't actually an Excel file."
+    wb_setup_good = openpyxl.Workbook()
+    sheetname_periodTiming = "Period Timing"
+    sheetname_cycleDaysList = "Cycle Days List"
+    sheetname_yearlySchedule = "Yearly Schedule"
+    ws_periodTiming = wb_good.create_sheet(sheetname_periodTiming)
+    ws_cycleDaysList = wb_good.create_sheet(sheetname_cycleDaysList)
+    ws_yearlySchedule = wb_good.create_sheet(sheetname_yearlySchedule)
+    for line in data_periodTiming:
+      ws_periodTiming.append(line)
+    ws_cycleDaysList.append(data_cycleDaysList)
+    for line in data_yearlySchedule:
+      ws_yearlySchedule(line)
+    wb_setup_bad = openpyxl.Workbook()
+    ws_bad = wb_setup_bad.active
+    for line in data_badExcel:
+      ws_bad.append(line)
 
     def setUp(self):
       self.setUpPyfakefs()
@@ -76,12 +116,10 @@ class Test_parse_schedule_setup_file(fake_filesystem_unittest.TestCase):
       os.mkdir(self.good_folder_path)
       os.mkdir(self.bad_folder_path)
       # Add files in fakefs
-
       for file in self.bad_folder_filename_list:
         bad_folder_filepath = "{}/{}".format(self.bad_folder_path, file)
         self.bad_folder_full_filepath_list.append(bad_folder_filepath)
         open(bad_folder_filepath, 'a').close()
-
       for file in self.good_folder_filename_list:
         good_folder_filepath = "{}/{}".format(self.good_folder_path, file)
         self.good_folder_full_filepath_list.append(good_folder_filepath)
@@ -92,8 +130,8 @@ class Test_parse_schedule_setup_file(fake_filesystem_unittest.TestCase):
         returning object containing setup dicts and lists"""
         pass
 
-    def test_raises_exception_if_invalid_path(self):
-        """If input string is not a valid folder path, throw ValueError"""
+    def test_raises_valueerror_if_invalid_path(self):
+        """If input string is not a valid folder path, raise ValueError"""
         # create an invalid folder path
         # pass into parseScheduleSetup and check for error raised
         self.assertRaisesRegex(
@@ -103,8 +141,8 @@ class Test_parse_schedule_setup_file(fake_filesystem_unittest.TestCase):
             self.not_a_folder_path
         )
 
-    def test_raises_exception_if_no_setup_file(self):
-        """If no Excel file matching preset filename exists, throw ValueError"""
+    def test_raises_valueerror_if_no_setup_file(self):
+        """If no Excel file matching preset filename exists, raise ValueError"""
         self.assertRaisesRegex(
             ValueError,
             'No schedule setup file found', # TODO: use string constant from code
@@ -112,14 +150,39 @@ class Test_parse_schedule_setup_file(fake_filesystem_unittest.TestCase):
             self.bad_folder_path
         )
 
+    def test_raises_valueerror_if_setup_file_not_excel(self):
+      """If setup file isn't an Excel file, raise ValueError"""
+      # test 1: just a text file
+      filepath = "{}/{}".format(
+        self.good_folder_path, cycle_calendar_generator.SCHEDULE_SETUP_FILENAME
+      )
+      with open(filepath) as file:
+        file.write(data_justATextDoc)
+      self.assertRaisesRegex(
+        ValueError,
+        'Not an Excel file',
+        cycle_calendar_generator.parseScheduleSetup,
+        self.filepath
+      )
 
-
-
+    def test_raises_valueerror_if_setup_file_unparseable(self):
+      """If Excel file can't be parsed following preset format, raise ValueError"""
+      # test 1: just a text file
+      filepath = "{}/{}".format(
+        self.good_folder_path, cycle_calendar_generator.SCHEDULE_SETUP_FILENAME
+      )
+      self.wb_setup_bad.save(filepath)
+      self.assertRaisesRegex(
+        ValueError,
+        'Setup file data does not match accepted format',
+        cycle_calendar_generator.parseScheduleSetup,
+        self.filepath
+      )
 
 
 
 # Parsing should generate:
-## [dict]periodTiming -> [int]periodNumber: [tuple(Date, Date)](startTime, endTime)
+## [dict]periodTiming -> [int]periodNumber: [tuple(Time, Time)](startTime, endTime)
 ## [list]CycleDaysList -> (str)cycleDay {showing all cycleDay strings}
 ## [dict]yearlySchedule -> [Date]date: [str]cycleDay
 # Check for teacher schedule Excel files
